3f5cdc7a93e2e7d2ca2d4078d8b7b302ffaea45b
Gaurav-krrocks <103325199+Gaurav-krrocks@users.noreply.github.com>
2022-04-09 20:56:54 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,20 @@
+class Demo
+{
+
+   static string name ="Shankar";
+   staticint age =54;
+   static double sal =90000.00;
+   public static void prncidetails ()
+   {
+   System.out.println ("the pricipal name is"+name);
+   System.out.println ("the pricipal age is"+age);
+   System.out.println ("the pricipal salary is"+sal);
+   }
+   publis static void main( String[]args)
+  {
+   demo.princiDetails();
+   PrinciDetails();
+
+   
+   }
+}
